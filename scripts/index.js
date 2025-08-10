94efebd276c2ef54e8c64efac9db30c4ad4f62c0
AnonymousA11 <mccullohj@gmail.com>
2025-08-10 19:04:39 -0400

    logged card dataset to console

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
+
 const editProfilebtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
@@ -11,7 +38,6 @@ const editProfileDescriptionInput = editProfileModal.querySelector(
 
 const newPostImageInput = document.querySelector("#image-link-input");
 const newPostCaptionInput = document.querySelector("#post-caption-input");
-
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
@@ -27,11 +53,11 @@ const profileDescription = document.querySelector(".profile__description");
 //Open/Close modal functionality
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
-};
+}
 
 function closeModal(modal) {
   modal.classList.remove("modal_is-opened");
-};
+}
 
 // Create the form submission handler for editing profile.
 function handleProfileFormSubmit(evt) {
@@ -77,8 +103,13 @@ newPostBtn.addEventListener("click", function () {
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-
   closeModal(newPostModal);
 });
 
 editProfileForm.addEventListener("submit", handleProfileFormSubmit);
+
+const loggedInitialCards = initialCards.forEach(function (card) {
+  console.log(card.name);
+});
+
+console.log(loggedInitialCards);
